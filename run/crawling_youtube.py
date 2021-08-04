6dbbb7533a6b185d6645872c08eccc7e0b7c348b
CuteSSu <ssumina96@gmail.com>
2021-08-04 15:07:55 +0900

    new opinion live file start

---
+++ run/crawling_youtube.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+df_raw = pd.read_excel('Practice/[백신]유튜브 댓글 크롤링 url.xlsx')
--- run/main_crawl_link.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-driver_path = '../resources/chromedriver' # driver path
-url = 'https://play.google.com/store/apps/top/category/GAME_EDUCATIONAL'
-
-browser = webdriver.Chrome(executable_path=driver_path) # Chrome driver
-browser.get(url)
-#
-browser.quit()
